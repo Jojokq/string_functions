32c57680dfddeb6800db84c782733358f6024686
Jojokq <111878744+Jojokq@users.noreply.github.com>
2022-08-29 17:24:32 +0300

    Added doxygen documentation

--- main.cpp
+++ main.cpp
@@ -1,37 +1,41 @@
+
+//----------------------------------------------------------------------------------------------------
+//! @brief Строковые функции
+//!
+//! @author Евгений Мелешенко
+//! @file main.cpp
+//! @date 26.08.2022
+//!
+//! @copyright Copyright (c) 2022
+//----------------------------------------------------------------------------------------------------
+
 #include <stdlib.h>
-
 #include "Functions.h"
-
 
 int main()
 {
-
-    //char* s1;
 
     char s[20] = "something";
     char s1[30] = "fadsfdafmdskfsuis";
 
     printf("puts : ");
-
     my_puts(s);
 
-    printf("\n strchr : %d\n", my_strchr(s, 'o') - s);
+    printf("\nstrchr : %d\n", my_strchr(s, 'o') - s);
 
     printf("strlen : %d\n", my_strlen(s));
 
-    //puts(s);
     printf("strcpy : ");
     my_puts(my_strcpy(s1, s));
-
 
     printf("\nstrncpy(3) : ");
     my_puts(my_strncpy(s1, s, 3));
 
     printf("\n strcat : ");
-    my_puts(my_strcat(s1, s)); // должно вернуть предыдущее копирование + новое (somsomething)
+    my_puts(my_strcat(s1, s));
 
     printf("\n strncat : ");
-    my_puts(my_strncat(s1,s,3)); //somsomethingsom
+    my_puts(my_strncat(s1,s,3));
 
     printf("\n strcmp : ");
     printf("%d", my_strcmp(s,s1));
